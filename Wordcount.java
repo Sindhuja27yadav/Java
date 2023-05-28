c7f6bfedf4b227b995be6f9d7d3afb172857b9ed
Sindhuja27yadav <122982478+Sindhuja27yadav@users.noreply.github.com>
2023-05-28 15:19:25 +0530

    Create Wordcount.java

---
+++ Wordcount.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class WordCount{
+    public static void main(String[] args){
+        String filename="path/to/your/file.txt";
+ // Specify the path to your text file  
+ try (BufferedReader reader=new BufferedReader(new FileReader(filename))) {
+   int wordCount = 0;
+   int charCount = 0;
+   int lineCount = 0;
+   String line;
+ while ((line = reader.readLine())!=null){
+    lineCount++;
+    String[] words=line.split("\\s+");
+    wordCount+=words.length;
+    charCount+=line.length();
+    }  
+   System.out.println("Number of words:"+wordCount);
+   System.out.println("Number of characters:"+ charCount);
+   System.out.println("Number of lines:"+lineCount);
+  }catch(IOException e) {
+     e.printStackTrace();
+      }
+    }
+  }
